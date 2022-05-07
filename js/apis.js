2683423ea0ebbd43afda43d95a0139aad376bed4
CesarCoder2021 <cvarettoni@gmail.com>
2022-05-07 18:01:18 -0300

    .

--- js/apis.js
+++ js/apis.js
@@ -3,21 +3,21 @@ const noticias = document.querySelector ("#noticias");
 // Llamada a la API de NewsData
 const sumarPost = async () => {
     const resp = await
-fetch ("https://newsdata.io/api/1/news?apikey=pub_7014cffa8aab19d57149825860b37fc0bda9&q=cryptocurrency")
+fetch ("http://api.mediastack.com/v1/news?access_key=b43b6966e94071f82b3d2acdde071467&keywords=salud+visual&languages=es");
 
-    const data = await resp.json()
+const data = await resp.json()
 // Creación de las cards con los contenidos del array de las noticias
-     data.results.forEach ((post) => {
+     data.data.forEach ((post) => {
         const div = document.createElement ("div")
         div.innerHTML += `
             <div class="card w-100">
                 <div class="card-body">
                     <h5 class="card-title">${post.title}</h5>
                     <p class="card-text">${post.description}</p>
-                    <a href="${post.link}" class="btn btn-primary">Ver más ...</a>
+                    <p class="card-text"><small class="text-muted">Origen: ${post.source}</small></p>
+                    <a href="${post.url}" class="btn btn-primary">Ver más ...</a>
                 </div>
             </div>
-            <p> </p>
         `
         noticias.append(div)
     })
